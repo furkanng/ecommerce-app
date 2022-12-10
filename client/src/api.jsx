0c3f267857f96358797005a86e55d7707bb1699c
Furkan Güzelgörür <furkanguzelgorur@gmail.com>
2022-12-11 02:15:17 +0300

    useInfiniteQuery used

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-export const fetchProductList = async () => {
-  const { data } = await axios.get(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/product`
-  );
+export const fetchProductList = async ({ pageParam = 1 }) => {
+	const { data } = await axios.get(
+		`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`
+	);
 
-  return data;
+	return data;
 };
 
 export const fetchProduct = async (id) => {
-  const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`);
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`
+  );
 
   return data;
 };
--- client/src/components/Featured/index.jsx
+++ client/src/components/Featured/index.jsx
@@ -1,34 +1,69 @@
 import React from "react";
 import styles from "./styles.module.css";
-import { Grid , Divider} from "@chakra-ui/react";
+import { Grid, Divider, Box, Flex, Button } from "@chakra-ui/react";
 import CardItem from "../Card/İndex";
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import { fetchProductList } from "../../api";
 
 function Featured() {
-  const { isLoading, error, data } = useQuery("products", fetchProductList);
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery("products", fetchProductList, {
+    getNextPageParam: (lastGroup, allGroups) => {
+      const morePagesExist = lastGroup?.length === 12;
 
-  if (isLoading) return "Loading...";
+      if (!morePagesExist) {
+        return;
+      }
+      return allGroups.length + 1;
+    },
+  });
 
-  if (error) return "An error has occurred: " + error.message;
+  if (status === "loading") return "Loading...";
 
-  console.log(data);
+  if (status === "error") return "An error has occurred: " + error.message;
 
   return (
     <div>
       <h1 className={styles.title}>Featured products</h1>
-      <Divider color="blue" width="100px"/>
-      <Divider color="blue" width="100px"/>
+      <Divider color="blue" width="100px" />
+      <Divider color="blue" width="100px" />
 
       <Grid
         templateColumns="repeat(4, 2fr)"
         templateRows="repeat(1, 1fr)"
         gap={3}
       >
-        {data.map((item, key) => (
-          <CardItem key={key} item={item} />
+        {data.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.map((item) => (
+              <Box w="100%" key={item._id}>
+                <CardItem item={item} />
+              </Box>
+            ))}
+          </React.Fragment>
         ))}
       </Grid>
+      <Flex mt="10" justifyContent="center">
+        <Button
+          onClick={() => fetchNextPage()}
+          isLoading={isFetchingNextPage}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </Button>
+        <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
+      </Flex>
     </div>
   );
 }
